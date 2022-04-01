1e29b20ce8bd51c3e54d1ec0cc635a770811b478
Sujinathakuri <sujchand2@gmail.com>
2022-04-01 10:42:37 -0400

    this is new project

---
+++ src/Strin.java
@@ -0,0 +1,21 @@
+
+public class Strin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "Hi I am Suzu";
+		String s1 = "Hey Love!";
+		
+		//System.out.println(s);
+		//System.out.println(s.charAt(5));
+		//System.out.println(s.substring(4));
+		//System.out.println(s.substring(5, 11));
+		System.out.println(s.contains(s1));
+		System.out.println(s.equals(s1));
+		
+		
+		
+
+	}
+
+}
